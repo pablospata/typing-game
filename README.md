9a46d140a9c8e257f5ab76aa895983185c288950
pablospata <pablospata@hotmail.com>
2023-07-14 19:35:48 -0300

    Primer commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Mini Proyecto de Typing Game en JavaScript
+
+Este proyecto es una simple aplicación de juego de mecanografía construida con JavaScript puro, HTML y CSS. Los usuarios tienen que escribir una cita generada aleatoriamente y se les proporciona retroalimentación en tiempo real sobre su precisión.
+
+## Código
+
+El código para este proyecto es bastante directo. Se utiliza la Fetch API para solicitar una cita aleatoria de una API. +
+
+Una vez que se recibe la cita, se separa en caracteres individuales y se muestra en la página. A medida que el usuario escribe, el juego compara cada caracter ingresado con el correspondiente en la cita. Si coincide, se marca como correcto. Si no coincide, se marca como incorrecto.
+
+## Prueba la App
+
+Para probar la app de tipeo, simplemente ve a la página de demostración alojada en GitHub Pages haciendo click en el siguiente enlace:
+
+* https://pablospata.github.io/typing-game-js/
---
+++ script.js
@@ -0,0 +1,71 @@
+const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random';
+const quoteDisplay = document.getElementById('quoteDisplay')
+const quoteInput = document.getElementById('quoteInput')
+const timer = document.getElementById('timer')
+
+function getRandomQuote() {
+    return fetch(RANDOM_QUOTE_API_URL)
+        .then(response => response.json())
+        .then(data => data.content)
+}
+
+async function renderNewQuote() {
+    let quote = await getRandomQuote();
+    quoteDisplay.innerHTML = '';
+    quoteInput.value = null;
+
+    quote = quote.split('');
+
+    quote.forEach(character => {
+        const characterSpan = document.createElement('span');
+        characterSpan.innerText = character;
+        quoteDisplay.appendChild(characterSpan);
+    });
+    
+    clearInterval(intervalId);
+    startTimer();
+}
+
+quoteInput.addEventListener('input', () => {
+    const arrayQuote = quoteDisplay.querySelectorAll('span');
+    const arrayValue = quoteInput.value.split('');
+
+    let correct = true;
+    arrayQuote.forEach((characterSpan, index) => {
+
+        if (arrayValue[index] == null) {
+            characterSpan.classList.remove('correct', 'incorrect');
+            correct = false;
+        } else if (arrayValue[index] === characterSpan.innerText) {
+            characterSpan.classList.add('correct');
+            characterSpan.classList.remove('incorrect');
+        } else {
+            characterSpan.classList.add('incorrect');
+            characterSpan.classList.remove('correct');
+            correct = false;
+        }
+    });
+
+    if (correct) {
+        alert('Congratulations!!!');
+        renderNewQuote();
+    }
+});
+
+let startTime;
+let intervalId;
+
+function startTimer() {
+    startTime = new Date();
+
+    intervalId = setInterval(() => {
+        let dateNow = new Date();
+        let difference = dateNow - startTime;
+
+        timer.innerText = Math.floor(difference / 1000);
+
+    }, 1000);
+
+}
+
+renderNewQuote();
